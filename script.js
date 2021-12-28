e5034b64215fe93dc869e9f184480c3c0c498431
ssmeds <stina_smeds@hotmail.com>
2021-12-28 22:45:43 +0100

    trying to unify our code and make sure it is responsive

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
 const BurgerEl = document.getElementById("burger");
-const NavItemsEl = document.getElementById("navItems")
+const NavItemsEl = document.getElementById("navItems");
+const menuOpen = document.createElement("div");
+menuOpen.classList.add("menuOpen");
+const overlay = document.querySelector('#overlay');
+overlay.appendChild(menuOpen);
 
-function burgerToggle(){
-    if(NavItemsEl.style.visibility == "hidden"){
+function burgerToggle() {
+    if (NavItemsEl.style.visibility == "hidden") {
         NavItemsEl.style.visibility = "visible";
     }
-    else{
+    else {
         NavItemsEl.style.visibility = "hidden";
-    } 
+    }
 }
 
-BurgerEl.addEventListener("click", ()=>{    
-    NavItemsEl.classList.toggle("active")   
+BurgerEl.addEventListener("click", () => {
+    console.log('click open');
+    // NavItemsEl.classList.toggle("active")
+    overlay.style.display = "block";
+    menuOpen.innerHTML = `<span id='closeBtn'>&#10005;</span>
+    <ul id="menuNavItems">
+            <li class="menuNavItem">Shop</li>
+            <li class="menuNavItem">Recycle denim</li>
+            <li class="menuNavItem">Contact us</li>
+            <li class="menuNavItem">FAQ</li>
+        </ul>`
+    document.querySelector('#closeBtn').addEventListener('click', () => { closeMenu() })
 })
 
+function closeMenu() {
+    console.log('click close');
+    document.querySelector('#overlay').style.display = "none";
+}
 
 /*function myFunction(x) {
     if (!x.matches) { // If media query matches
         NavItemsEl.style.visibility = "visible";
         console.log(x);
-    } 
+    }
   }
-  
+
   var x = window.matchMedia("(max-width: 768px)")
   myFunction(x) // Call listener function at run time
   x.addListener(myFunction) // Attach listener function on state changes*/
+
+
