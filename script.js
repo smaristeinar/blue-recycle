aa498dea8515a2a26b23dc45f6debe8790ad01a4
ssmeds <stina_smeds@hotmail.com>
2021-12-29 11:20:30 +0100

    removed some comments and fixed the script file

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const menuOpen = document.createElement("div");
 menuOpen.classList.add("menuOpen");
 const overlay = document.querySelector('#overlay');
 overlay.appendChild(menuOpen);
+const closeBtn = document.querySelector('#closeBtn');
 
 function burgerToggle() {
     if (NavItemsEl.style.visibility == "hidden") {
@@ -15,33 +16,12 @@ function burgerToggle() {
 }
 
 BurgerEl.addEventListener("click", () => {
-    console.log('click open');
-    // NavItemsEl.classList.toggle("active")
     overlay.style.display = "block";
-    menuOpen.innerHTML = `<span id='closeBtn'>&#10005;</span>
-    <ul id="menuNavItems">
-            <li class="menuNavItem">Shop</li>
-            <li class="menuNavItem">Recycle denim</li>
-            <li class="menuNavItem">Contact us</li>
-            <li class="menuNavItem">FAQ</li>
-        </ul>`
-    document.querySelector('#closeBtn').addEventListener('click', () => { closeMenu() })
 })
 
-function closeMenu() {
-    console.log('click close');
-    document.querySelector('#overlay').style.display = "none";
-}
+closeBtn.addEventListener('click', () => {
+    overlay.style.display = "none";
+})
 
-/*function myFunction(x) {
-    if (!x.matches) { // If media query matches
-        NavItemsEl.style.visibility = "visible";
-        console.log(x);
-    }
-  }
-
-  var x = window.matchMedia("(max-width: 768px)")
-  myFunction(x) // Call listener function at run time
-  x.addListener(myFunction) // Attach listener function on state changes*/
 
 
