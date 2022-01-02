09857591f619a8032137329d6e0b11075ec71d75
unknown <zlinus.zachrison@gmail.com>
2022-01-02 21:31:10 +0100

    adding basic functionality so you can remove cookie banner when clicking accept

--- script.js
+++ script.js
@@ -2,31 +2,36 @@ const BurgerEl = document.getElementById("burger");
 const NavItemsEl = document.getElementById("navItems");
 const menuOpen = document.createElement("div");
 menuOpen.classList.add("menuOpen");
-const overlay = document.querySelector('#overlay');
+const overlay = document.querySelector("#overlay");
 overlay.appendChild(menuOpen);
-const closeBtn = document.querySelector('#closeBtn');
-
+const closeBtn = document.querySelector("#closeBtn");
+const cookieContainer = document.getElementById("cookieContainer");
+const cookieBtn = document.getElementById("cookieBtn");
 
 BurgerEl.addEventListener("click", () => {
-    overlay.style.display = "block";
-})
+  overlay.style.display = "block";
+});
 
-closeBtn.addEventListener('click', () => {
-    overlay.style.display = "none";
-})
+closeBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
 
-BurgerEl.addEventListener("focus",() =>{
-    BurgerEl.addEventListener("keydown",(e)=>{
-        if(e.code == "enter"){overlay.style.display = "block";}
-    })
-})
+BurgerEl.addEventListener("focus", () => {
+  BurgerEl.addEventListener("keydown", (e) => {
+    if (e.code == "enter") {
+      overlay.style.display = "block";
+    }
+  });
+});
 
-closeBtn.addEventListener("focus",() =>{
-    closeBtn.addEventListener("keydown",(e)=>{
-        if(e.code == "enter"){overlay.style.display = "none";}
-    })
-})
+closeBtn.addEventListener("focus", () => {
+  closeBtn.addEventListener("keydown", (e) => {
+    if (e.code == "enter") {
+      overlay.style.display = "none";
+    }
+  });
+});
 
-
-
-
+cookieBtn.addEventListener("click", function () {
+  cookieContainer.remove();
+});
